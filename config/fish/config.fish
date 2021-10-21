60a87c999d2c841d2eb690949ddc0b5591b64828
Joe Lewis <joe@informedk12.com>
2021-10-21 12:19:47 -0800

    use alias instead of function for aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,23 +2,9 @@ fish_vi_key_bindings
 
 set -x EDITOR nvim
 
-function gv
-    mvim $argv
-end
+alias vim nvim
+alias weather 'curl wttr.in/ketchikan'
 
-function vim
-    nvim $argv
-end
-
-#function grep
-#    ggrep $argv
-#end
-
-function weather
-    curl wttr.in/pdx
-end
-
-#set -gx PATH /usr/bin $HOME/bin $HOME/.jenv/bin $PATH
 set -gx PATH $PATH $HOME/bin $HOME/.jenv/bin
 
 if functions --query 'machine_specific'
