239c5480589341b00aded445a13f471931f1a0ea
Joe Lewis <robotbill@gmail.com>
2021-11-27 21:25:39 -0900

    setup kitty

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,8 +2,11 @@ fish_vi_key_bindings
 
 set -x EDITOR nvim
 
+set -x KITTY_CONFIG_DIRECTORY $HOME/.config/kitty/kitty.conf
+
 alias vim nvim
 alias weather 'curl wttr.in/ketchikan'
+alias icat 'kitty +kitten icat'
 
 set -gx PATH $PATH $HOME/bin $HOME/.jenv/bin
 
